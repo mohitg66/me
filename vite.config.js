9c413a083920baa244e202780f5d7b39a6a8b8f5
Mohit <mohit21339@iiitd.ac.in>
2023-04-03 12:39:28 +0530

    removed dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
