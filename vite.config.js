69775caabadf2f26a4a0d8f9cb7401b7b4b6747f
Mohit <mohit21339@iiitd.ac.in>
2023-04-03 13:11:26 +0530

    added assetsInclude in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/me/',
+  assetsInclude: ['src/assets', 'src/assets/**/*', 'src/assets/**/*.*']
 })
