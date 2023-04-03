70e161ef365c70a12276ed5b73b62a558e59352f
Mohit <mohit21339@iiitd.ac.in>
2023-04-03 12:56:56 +0530

    added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/me/',
 })
-
